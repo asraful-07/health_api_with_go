10bfd65da4fefba9296e0766fdf5d933f0ba6e15
asraful-07 <asraful0508@gmail.com>
2026-05-20 10:59:27 +0600

    add put and delete

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 type User struct {
@@ -34,6 +35,10 @@ func main() {
 	mux.HandleFunc("GET /", rootHandler)
 	mux.HandleFunc("POST /users", createUserHandler)
 	mux.HandleFunc("GET /users", getsUsersHandler)
+    mux.HandleFunc("GET /user/{id}", getUserHandler)
+    mux.HandleFunc("PUT /user/{id}", updateUserHandler)
+    mux.HandleFunc("DELETE /user/{id}", deleteUserHandler)
+
 	fmt.Println("Server running on :8080")
 	 err := http.ListenAndServe(":8080", mux)
 	 
@@ -44,8 +49,7 @@ func main() {
 
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World!"))	
-	
+	w.Write([]byte("Health checker web site!"))	
 }
 
 func createUserHandler(w http.ResponseWriter, r *http.Request) {
@@ -67,5 +71,73 @@ func createUserHandler(w http.ResponseWriter, r *http.Request) {
 
 func getsUsersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(users)
+}
+
+func getUserHandler(w http.ResponseWriter, r *http.Request) {
+idParam := r.PathValue("id")
+
+id, err := strconv.Atoi(idParam)
+if err != nil {
+	http.Error(w, "Invalid user ID", http.StatusBadRequest)
+	return
+}
+for _, user := range users {
+	if user.Id == id {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(user)
+		return
+	}
+}
+http.Error(w, "User not found", http.StatusNotFound)
+}
+
+func updateUserHandler(w http.ResponseWriter, r *http.Request) {
+idParam := r.PathValue("id")
+id, err := strconv.Atoi(idParam)
+if err != nil {
+	http.Error(w, "Invalid user ID", http.StatusBadRequest)
+	return
+}
+
+var updatedUser User
+err = json.NewDecoder(r.Body).Decode(&updatedUser)
+if err != nil {
+	http.Error(w, "Invalid request payload", http.StatusBadRequest)
+	return
+}
+
+for i, user := range users {
+	if user.Id == id {
+		updatedUser.Id = id
+		users[i] = updatedUser	
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(updatedUser)
+		return
+	}
+}
+http.Error(w, "User not found", http.StatusNotFound)
+}
+
+
+func deleteUserHandler(w http.ResponseWriter, r *http.Request) {
+idParam := r.PathValue("id")
+id, err := strconv.Atoi(idParam)
+if err != nil {
+	http.Error(w, "Invalid user ID", http.StatusBadRequest)
+	return
+}
+for i, user := range users {
+	if user.Id == id {
+		users = append(users[:i], users[i+1:]...)
+
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}	
+		
+}
+http.Error(w, "User not found", http.StatusNotFound)
 }
